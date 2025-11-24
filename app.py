c7a0b31b36b9815c5039a3f36b531f80c2a3697b
vitordsoliveira <vitordaniel.silva32@gmail.com>
2025-11-24 11:11:16 -0300

    colocando nova mensagem

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
 
 @app.route('/')
 def home():
-    return jsonify(message="API is running")
+    return jsonify(message="API is running com deploy automatico")
 
 @app.route('/items', methods=['GET'])
 def get_items():
